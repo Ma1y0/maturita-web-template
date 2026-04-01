575a26daec7db9b34526ccd1c217e688cb0ab084
Ma1y0 <matyas.barr@gmail.com>
2026-04-01 11:27:17 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center gap-6 p-6">
+      <h1 className="text-3xl font-semibold">Hello, World!</h1>
+    </main>
+  );
+}
+
+export default App;
